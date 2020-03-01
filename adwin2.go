28e937784b669927280f74e44cb4efa6197c82ab
monochromegane <dev.kuro.obi@gmail.com>
2020-03-02 01:10:42 +0900

    Implemented practical epsilon for adwin2.

--- adwin2.go
+++ adwin2.go
@@ -3,23 +3,37 @@ package adwin
 import "math"
 
 type Adwin2 struct {
-	delta  float64
-	window *ExpHist
+	delta   float64
+	window  *ExpHist
+	window2 *ExpHist
+
+	conservative bool
 }
 
 func NewAdwin2(delta float64) *Adwin2 {
 	return &Adwin2{
-		delta:  delta,
-		window: NewExpHist(2),
+		delta:   delta,
+		window:  NewExpHist(2),
+		window2: NewExpHist(2),
 	}
+}
+
+func (a *Adwin2) Conservative(t bool) {
+	a.conservative = t
 }
 
 func (a *Adwin2) Add(x float64) {
 	a.window.Add(x)
+	if !a.conservative {
+		a.window2.Add(x * x)
+	}
 
 	detected := a.detectChanging()
 	if detected {
 		a.window.Drop()
+		if !a.conservative {
+			a.window2.Drop()
+		}
 	}
 }
 
@@ -35,7 +49,16 @@ func (a *Adwin2) detectChanging() bool {
 	n0 := n - n1
 	w0Sum := wSum - w1Sum
 
-	e := epsilonConservative(a.delta, n, n0, n1)
+	e := 0.0
+	if a.conservative {
+		e = epsilonConservative(a.delta, n, n0, n1)
+	} else {
+		v := a.window2.Sum()/float64(a.window2.Size()) - ((wSum / float64(n)) * (wSum / float64(n)))
+		if v == 0.0 {
+			v = 1e-8
+		}
+		e = epsilon(v, a.delta, n, n0, n1)
+	}
 	uHatW0 := w0Sum / float64(n0)
 	uHatW1 := w1Sum / float64(n1)
 	return math.Abs(uHatW0-uHatW1) >= e
